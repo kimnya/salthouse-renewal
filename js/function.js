6fec3d9c5cdd4f26ce9e6942ae7b824e6d360e45
김냐 <ymca582@cloud.com>
2023-10-06 10:24:42 +0900

    솔트하우스 v1.1

--- js/function.js
+++ js/function.js
@@ -1,147 +1,148 @@
 //product-best pagination
 $(function () {
-  const $menuItem = $(".menu-item");
-  const $indicater = $(".menu>li>a");
-  const $prevBtn = $(".product-best .prev");
-  const $nextBtn = $(".product-best .next");
+	const $menuItem = $('.menu-item');
+	const $indicater = $('.menu>li>a');
+	const $prevBtn = $('.product-best .prev');
+	const $nextBtn = $('.product-best .next');
 
-  let nowIdx = 0;
-  $indicater.on("click", function (evt) {
-    evt.preventDefault();
-    nowIdx = $indicater.index(this);
-    $menuItem.fadeOut(200);
-    $menuItem.eq(nowIdx).fadeIn(200).css({
-      display: "flex",
-    });
-    console.log(nowIdx);
-    $indicater.eq(nowIdx).parent().addClass("on").siblings().removeClass("on");
-  });
+	let nowIdx = 0;
+	$indicater.on('click', function (evt) {
+		evt.preventDefault();
+		nowIdx = $indicater.index(this);
+		$menuItem.fadeOut(200);
+		$menuItem.eq(nowIdx).fadeIn(200).css({
+			display: 'flex',
+		});
+		console.log(nowIdx);
+		$indicater.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+	});
 
-  $nextBtn.on("click", function (evt) {
-    evt.preventDefault();
-    // oldIdx = nowIdx;
-    if (nowIdx < $indicater.length - 1) {
-      nowIdx++;
-    } else {
-      nowIdx = 0;
-    }
-    console.log(nowIdx);
+	$nextBtn.on('click', function (evt) {
+		evt.preventDefault();
+		// oldIdx = nowIdx;
+		if (nowIdx < $indicater.length - 1) {
+			nowIdx++;
+		} else {
+			nowIdx = 0;
+		}
+		console.log(nowIdx);
 
-    $indicater
-      .eq(nowIdx)
-      .parent()
-      .css({
-        display: "block",
-      })
-      .siblings()
-      .css({
-        display: "none",
-      });
+		$indicater
+			.eq(nowIdx)
+			.parent()
+			.css({
+				display: 'block',
+			})
+			.siblings()
+			.css({
+				display: 'none',
+			});
 
-    $menuItem.fadeOut(200);
-    $menuItem.eq(nowIdx).fadeIn(200).css({
-      display: "flex",
-    });
-  });
+		$menuItem.fadeOut(200);
+		$menuItem.eq(nowIdx).fadeIn(200).css({
+			display: 'flex',
+		});
+	});
 
-  $prevBtn.on("click", function (evt) {
-    evt.preventDefault();
-    // oldIdx = nowIdx;
-    if (nowIdx > 0) {
-      nowIdx--;
-    } else {
-      nowIdx = $indicater.length - 1;
-    }
+	$prevBtn.on('click', function (evt) {
+		evt.preventDefault();
+		// oldIdx = nowIdx;
+		if (nowIdx > 0) {
+			nowIdx--;
+		} else {
+			nowIdx = $indicater.length - 1;
+		}
 
-    console.log(nowIdx);
+		console.log(nowIdx);
 
-    $indicater
-      .eq(nowIdx)
-      .parent()
-      .css({
-        display: "block",
-      })
-      .siblings()
-      .css({
-        display: "none",
-      });
+		$indicater
+			.eq(nowIdx)
+			.parent()
+			.css({
+				display: 'block',
+			})
+			.siblings()
+			.css({
+				display: 'none',
+			});
 
-    $menuItem.fadeOut(200);
-    $menuItem.eq(nowIdx).fadeIn(200).css({
-      display: "flex",
-    });
-  });
+		$menuItem.fadeOut(200);
+		$menuItem.eq(nowIdx).fadeIn(200).css({
+			display: 'flex',
+		});
+	});
 });
 
 // scroll
 $(function () {
-  const $renewal = $(".renewal a");
-  $(window).scroll(function () {
-    scrollTop = $(document).scrollTop();
-    console.log($(document).scrollTop());
-    if (scrollTop > 50) {
-      $renewal.stop().animate(
-        {
-          height: 0,
-        },
-        200
-      );
-    } else {
-      $renewal.stop().animate(
-        {
-          height: "50px",
-        },
-        100
-      );
-    }
-  });
+	const $renewal = $('.renewal a');
+	$(window).scroll(function () {
+		whith = $(document).whith();
+		scrollTop = $(document).scrollTop();
+		console.log($(document).scrollTop());
+		if (scrollTop > 50 || whith < 650) {
+			$renewal.stop().animate(
+				{
+					height: 0,
+				},
+				200
+			);
+		} else {
+			$renewal.stop().animate(
+				{
+					height: '50px',
+				},
+				100
+			);
+		}
+	});
 
-  const $top = $(".top>a");
-  $top.on("click", function (evt) {
-    evt.preventDefault();
-    $("html,body").animate({ scrollTop: 0 }, 300);
-  });
+	const $top = $('.top>a');
+	$top.on('click', function (evt) {
+		evt.preventDefault();
+		$('html,body').animate({ scrollTop: 0 }, 300);
+	});
 });
 
 //hambuger menu
 $(function () {
-  const $hambuger = $("nav>p>a");
-  const $gnb = $(".hambugerMenu");
-  const $light = $(".light");
-  const $tabMenu = $(".gnb_hambuger>li");
+	const $hambuger = $('nav>p>a');
+	const $gnb = $('.hambugerMenu');
+	const $light = $('.light');
+	const $tabMenu = $('.gnb_hambuger>li');
 
-  $hambuger.on("click", function (evt) {
-    evt.preventDefault();
-    $gnb.addClass("on");
-    $light.addClass("on");
-  });
-  $(window).on("resize", function () {
-    $gnb.removeClass("on");
-    $light.removeClass("on");
-  });
-  $light.on("click", function () {
-    $gnb.removeClass("on");
-    $light.removeClass("on");
-    $tabMenu.first().children("a").text("카테고리");
-  });
+	$hambuger.on('click', function (evt) {
+		evt.preventDefault();
+		$gnb.addClass('on');
+		$light.addClass('on');
+	});
+	$(window).on('resize', function () {
+		$gnb.removeClass('on');
+		$light.removeClass('on');
+	});
+	$light.on('click', function () {
+		$gnb.removeClass('on');
+		$light.removeClass('on');
+		$tabMenu.first().children('a').text('카테고리');
+	});
 });
 
 //메뉴체크
 $(function () {
-  const $shopOn = $(".shopMenu");
-  const $tabMenu = $(".gnb_hambuger>li");
-  $shopOn.children().on("click", function (evt) {
-    evt.preventDefault();
-    if ($(this).index() === 1) {
-      $tabMenu.first().children("a").text("소금집델리");
-      $(this).addClass("on").siblings().removeClass("on");
-    } else {
-      $tabMenu.first().children("a").text("카테고리");
-      $(this).addClass("on").siblings().removeClass("on");
-    }
-  });
+	const $shopOn = $('.shopMenu');
+	const $tabMenu = $('.gnb_hambuger>li');
+	$shopOn.children().on('click', function (evt) {
+		evt.preventDefault();
+		if ($(this).index() === 1) {
+			$tabMenu.first().children('a').text('소금집델리');
+			$(this).addClass('on').siblings().removeClass('on');
+		} else {
+			$tabMenu.first().children('a').text('카테고리');
+			$(this).addClass('on').siblings().removeClass('on');
+		}
+	});
 
-  $tabMenu.on("click", function () {
-    $(this).toggleClass("on").children("i").toggleClass("fa-solid fa-angle-up");
-  });
+	$tabMenu.on('click', function () {
+		$(this).toggleClass('on').children('i').toggleClass('fa-solid fa-angle-up');
+	});
 });
